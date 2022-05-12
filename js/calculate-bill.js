03e2fda91a055dbf3d305f1e4be860b1488b9338
Mxolisi <nyawose57@gmail.com>
2022-05-12 04:35:52 +0200

    made changes to prevent any new costs from being added on setting-bill widget

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -19,13 +19,14 @@ function calculateBtnClicked(){
     // get the string entered in the textArea
     var billString = billStringElement.value;
 
-    //split the string
-    var billItems = billString.split(",")
+//  * this function should read the string value entered - split it on a comma.
+var billItems = billString.split(",")
     // a variable for the total phone bill.
     var billTotal = 0;
-    //loop over all the bill items
-    for (var i=0;i<billItems.length;i++){
+//  * loop over all the entries in the the resulting list
+for (var i=0;i<billItems.length;i++){
         var billItem = billItems[i].trim();
+        
         if (billItem.toLowerCase()=== "call"){
             billTotal += 2.75;
         }
@@ -35,11 +36,11 @@ function calculateBtnClicked(){
 
         
     }
-    
+    //  * once done looping over all the entries - display the total onto the screen in the billTotal element
     //round to two decimals
     var roundedBillTotal = billTotal.toFixed(2);
    // billTotalElement.innerHTML = roundedBillTotal;
-
+// back to colour default after the danger and warning
     billTotalElement.classList.remove("warning")
     billTotalElement.classList.remove("danger")
 
@@ -55,24 +56,13 @@ function calculateBtnClicked(){
 
         //     billTotalElement.classList.add('danger')
 
-
         // }
         billTotalElement.innerHTML = roundedBillTotal;
 }
-
-
-
-
-
-   
-
+//link the function to a click event on the calculate button
 
 calculateBtn.addEventListener('click', calculateBtnClicked);
 
-//create the function that will be called when the calculate button is pressed
-//  * this function should read the string value entered - split it on a comma.
-//  * loop over all the entries in the the resulting list
-//  * check if it is a call or an sms and add the right amount to the overall total
-//  * once done looping over all the entries - display the total onto the screen in the billTotal element
 
-//link the function to a click event on the calculate button
+
+
